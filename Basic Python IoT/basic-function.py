039b5eacf79170db39bc218f5a4b73e0f04c7781
Hery <thitphavanh.psv@gmail.com>
2022-05-11 21:33:25 +0700

    update

---
+++ Basic Python IoT/basic-function.py
@@ -0,0 +1,19 @@
+def hello():
+	print('Hello my friend')
+
+
+def sabaiydee():
+	print('ສະບາຍດີໝູ່')
+
+def nihao():
+	print('ໜີຫ້າວ')
+
+friend = 'China'
+
+if friend == 'England':
+	hello()
+elif friend == 'China':
+	nihao()
+else:
+	sabaiydee()
+
