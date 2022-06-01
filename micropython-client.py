22012af1eb35dfa00353bd5e525280bf6795e39f
Hery <thitphavanh.psv@gmail.com>
2022-06-01 20:57:38 +0700

    update

--- micropython-client.py
+++ micropython-client.py
@@ -5,10 +5,10 @@ port = 9000
 
 
 def send_data(data):
-    server = socket.socket()
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.connect((serverip, port))
-    server.send(data.encode('utf-8'))
-    data_server = server.recv(1024).decode('utf-8')
-    print('Server:', data_server)
-    server.close()
+	server = socket.socket()
+	server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	server.connect((serverip, port))
+	server.send(data.encode('utf-8'))
+	data_server = server.recv(1024).decode('utf-8')
+	print('Server:', data_server)
+	server.close()
--- micropython-server.py
+++ micropython-server.py
@@ -7,16 +7,16 @@ port = 9000
 buffsize = 4096
 
 while True:
-    server = socket.socket()
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind((serverip, port))
-    server.listen(1)
-    print('waiting micropython...')
+	server = socket.socket()
+	server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	server.bind((serverip, port))
+	server.listen(1)
+	print('waiting micropython...')
 
-    client, addr = server.accept()
-    print('connected from:', addr)
+	client, addr = server.accept()
+	print('connected from:', addr)
 
-    data = client.recv(buffsize).decode('utf-8')
-    print('Data from MicroPython: ', data)
-    client.send('received your messages.'.encode('utf-8'))
-    client.close()
+	data = client.recv(buffsize).decode('utf-8')
+	print('Data from MicroPython: ', data)
+	client.send('received your messages.'.encode('utf-8'))
+	client.close()
