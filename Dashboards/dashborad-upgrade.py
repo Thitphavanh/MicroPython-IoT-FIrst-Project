afc49e8c1a9f5b003b50d1845cfd6076e2eb1685
Hery <thitphavanh.psv@gmail.com>
2022-06-29 15:52:44 +0700

    update

--- Dashboards/dashborad-upgrade.py
+++ Dashboards/dashborad-upgrade.py
@@ -5,7 +5,7 @@ from threading import Thread
 import time
 
 GUI = Tk()
-GUI.geometry('801x601')
+GUI.geometry('1000x636')
 GUI.title('SMART IOT BY ANITOCORN INC.')
 GUI.state('zoomed')
 
@@ -20,7 +20,7 @@ canvas.create_image(300, 200, anchor=NW, image=background)
 # ໃສ່ຫຼາຍຫຼ່ຽມ square
 canvas.create_polygon([630,426,675,450,675,495,629,470], fill='#01262e', width=1, outline=None, tags='d1')
 # ໃສ່ຂໍ້ຄວາມ text
-canvas.create_text(300,200, text='Window is going to open', fill='black', font=('bold', 20), tags='d1')
+canvas.create_text(300,200, text='Window is going to close', fill='#01262e', font=('bold', 20), tags='d1')
 # ໃສ່ເສັ້ນ line
 canvas.create_line(425,320,640,455, fill='grey', width=1, tags='d1')
 # canvas.create_line(150,50,120,100,50,0,0,50,smooth=1)
@@ -35,12 +35,12 @@ def DoorOnOff(event):
     door_state = not door_state
     canvas.delete('d1')
     if door_state == True:
-        canvas.create_polygon([630,426,675,450,675,495,629,470], fill='#e8c307', width=1, outline=None, tags='d1')
-        canvas.create_text(300, 200, text='Window is going to open', fill='black', font=('bold', 20), tags='d1')
+        canvas.create_polygon([630,426,675,450,675,495,629,470], fill='#01262e', width=1, outline=None, tags='d1')
+        canvas.create_text(300, 200, text='Window is going to close', fill='#01262e', font=('bold', 20), tags='d1')
         canvas.create_line(425,320,640,455, fill='grey', width=1, tags='d1')
     else:
-        canvas.create_polygon([630,426,675,450,675,495,629,470], fill='#01262e', width=1, outline=None, tags='d1')
-        canvas.create_text(300, 200, text='Window is going to close', fill='black', font=('bold', 20), tags='d1')
+        canvas.create_polygon([630,426,675,450,675,495,629,470], fill='#e8c307', width=1, outline=None, tags='d1')
+        canvas.create_text(300, 200, text='Window is going to open', fill='#e8c307', font=('bold', 20), tags='d1')
         canvas.create_line(425,320,640,455, fill='grey', width=1, tags='d1')
 
 
@@ -50,23 +50,40 @@ canvas.create_image(1063, 461, image=fan, tags='img3', anchor=CENTER)
 angle = 0
 
 
-def run_fan(event=None):
-    # fan = ImageTk.PhotoImage(resize_image('fan-icon.png',100))
-    global angle
-    while True:
-        if angle != 0:
-            canvas.delete('img3')
-            fan = ImageTk.PhotoImage(image=Image.open('fan.png').rotate(angle))
-            canvas.create_image(1063, 461, image=fan, tags='img3', anchor=CENTER)
-        angle += 45
-        if angle >= 360:
-            angle = 0
-        time.sleep(0.1)
+fan_state = True
 
+def run_fan(event=None):
+	# fan = ImageTk.PhotoImage(resize_image('fan-icon.png',100))
+	global angle
+	while True:
+		if fan_state:
+			canvas.delete('img3')
+			fan = ImageTk.PhotoImage(image = Image.open('fan.png').rotate(angle)) 
+			canvas.create_image(1063,461,image=fan,tags='img3',anchor=CENTER)
+			angle += 30
+			# if angle > 360:
+			# 	angle = 0
+		else:
+			fan = ImageTk.PhotoImage(image = Image.open('fan.png').rotate(angle)) 
+			canvas.create_image(1063,461,image=fan,tags='img3',anchor=CENTER)
+
+		time.sleep(0.1)
+	print('break')
 
 task = Thread(target=run_fan)
 task.start()
 
+def reset_fan(event):
+	global fan_state
+	fan_state = not fan_state
+	try:
+		if fan_state:
+			canvas.itemconfig('img3',state='normal')
+		else:
+			canvas.itemconfig('img3',state='hidden')
+	except:
+		pass
 
+GUI.bind('<F12>',reset_fan)
 GUI.bind('<Return>', DoorOnOff)
 GUI.mainloop()
