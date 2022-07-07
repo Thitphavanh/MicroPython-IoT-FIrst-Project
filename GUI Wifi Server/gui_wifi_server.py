95a971ad80ba74bcbfba9b9b16847ade3c436d7c
Hery <thitphavanh.psv@gmail.com>
2022-07-07 11:48:30 +0700

    update

--- GUI Wifi Server/gui_wifi_server.py
+++ GUI Wifi Server/gui_wifi_server.py
@@ -23,28 +23,47 @@ def runserver():
         # data = 'LED1:ON' or 'LED1:OFF'
         data_split = data.split(':')
 
-        if float(data_split[1]) > 32:
-            img = PhotoImage(file='level3.png')
+        if float(data_split[1]) > 28:
+            img = PhotoImage(file='28.png')
             ICON.configure(image=img)
             ICON.image = img
-            v_status.set('{} temperature is {} °C'.format(data_split[0], data_split[1]))
-        elif float(data_split[1]) > 28.5:
-            img = PhotoImage(file='level2.png')
+            v_status.set('{} temperature is {} °C'.format(
+                data_split[0], data_split[1]))
+        elif float(data_split[1]) > 27:
+            img = PhotoImage(file='27.png')
             ICON.configure(image=img)
             ICON.image = img
-            v_status.set('{} temperature is {} °C'.format(data_split[0], data_split[1]))
-        elif float(data_split[1]) > 25.5:
-            img = PhotoImage(file='level1.png')
+            v_status.set('{} temperature is {} °C'.format(
+                data_split[0], data_split[1]))
+        elif float(data_split[1]) > 26:
+            img = PhotoImage(file='26.png')
             ICON.configure(image=img)
             ICON.image = img
-            v_status.set('{} temperature is {} °C'.format(data_split[0], data_split[1]))
+            v_status.set('{} temperature is {} °C'.format(
+                data_split[0], data_split[1]))
+        elif float(data_split[1]) > 25:
+            img = PhotoImage(file='25.png')
+            ICON.configure(image=img)
+            ICON.image = img
+            v_status.set('{} temperature is {} °C'.format(
+                data_split[0], data_split[1]))
+        elif float(data_split[1]) > 24:
+            img = PhotoImage(file='24.png')
+            ICON.configure(image=img)
+            ICON.image = img
+            v_status.set('{} temperature is {} °C'.format(
+                data_split[0], data_split[1]))
+        elif float(data_split[1]) > 23:
+            img = PhotoImage(file='23.png')
+            ICON.configure(image=img)
+            ICON.image = img
+            v_status.set('{} temperature is {} °C'.format(
+                data_split[0], data_split[1]))
         else:
-            img = PhotoImage(file='level1.png')
+            img = PhotoImage(file='23.png')
             ICON.configure(image=img)
             ICON.image = img
             v_status.set('Temperature is cold')
-            
-
 
         # if data_split[1] == 'ON':
         #     v_status.set('{} status is {} '.format(data_split[0], data_split[1]))
@@ -73,8 +92,8 @@ label2 = Label(GUI, textvariable=v_status, font=FONT1)
 label2.configure(fg='red')
 label2.pack()
 
-img = PhotoImage(file='level1.png')
-ICON = Label(GUI,image=img)
+img = PhotoImage(file='28.png')
+ICON = Label(GUI, image=img)
 ICON.pack()
 
 # -------------runserver-------------
